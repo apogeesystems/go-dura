e12c8cf9b4361d12fd62f2f2ea90f17edb88b6ca
Dane Nelson <dnelson0756@gmail.com>
2022-01-17 13:19:23 -0700

    added package-level runtimeLock, altered the viper instance to unmarshal to runtimeLock.

--- cmd/dura/database.go
+++ cmd/dura/database.go
@@ -1,6 +1,7 @@
 package dura
 
 import (
+	"fmt"
 	"github.com/spf13/viper"
 	"log"
 	"os"
@@ -8,9 +9,10 @@ import (
 )
 
 var (
-	dbViper   *viper.Viper
-	cacheHome string
-	cacheFile string
+	dbViper     *viper.Viper
+	cacheHome   string
+	cacheFile   string
+	runtimeLock RuntimeLock
 )
 
 func InitDatabase() {
@@ -27,17 +29,46 @@ func InitDatabase() {
 	} else {
 		cacheHome = strings.TrimRight(cacheHome, "/") + "/.cache/dura"
 	}
+	fmt.Printf("Cache home: '%s'\n", cacheHome)
 
 	// Search config in home directory with name ".go-dura" (without extension).
 	dbViper.AddConfigPath(cacheHome)
 	dbViper.SetConfigType("json")
-	dbViper.SetConfigName("runtime.db")
-	cacheFile = strings.TrimRight(cacheHome, "/") + "/runtime.db"
+	dbViper.SetConfigName("runtime")
+	cacheFile = strings.TrimRight(cacheHome, "/") + "/runtime"
+	dbViper.Set("pid", nil)
 
+	readInRuntime(nil)
+}
+
+func readInRuntime(path *string) (err error) {
+	if path == nil {
+		if err = dbViper.ReadInConfig(); err == nil {
+			fmt.Fprintln(os.Stderr, "Runtime file loaded: ", viper.ConfigFileUsed())
+		} else {
+			return
+		}
+	} else {
+		var file *os.File
+		if file, err = os.Open(*path); err != nil {
+			return
+		}
+		if err = dbViper.ReadConfig(file); err != nil {
+			return
+		}
+	}
+	//fmt.Printf("%+v", viper.Get("repos"))
+	if err = dbViper.Unmarshal(&runtimeLock); err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("%+v\n\n", config)
+	//fmt.Printf("%s\n%s\n%+v\n\n", *config.Commit.Author, *config.Commit.Email, config.Commit.ExcludeGitConfig)
+	//fmt.Printf("%+v\n\n", config.Repositories)
+	return
 }
 
 type RuntimeLock struct {
-	Pid *uint32
+	Pid *uint32 `json:"pid,omitempty" mapstructure:"pid,omitempty"`
 }
 
 func (rl *RuntimeLock) Empty() {
@@ -53,18 +84,16 @@ func (rl *RuntimeLock) GetDuraCacheHome() (path string) {
 }
 
 func (rl *RuntimeLock) Load() (err error) {
-	return rl.LoadFile(cacheFile)
+	//fmt.Printf("Loading runtime cache file: '%s'\n", cacheFile)
+	return readInRuntime(nil)
 }
 
 func (rl *RuntimeLock) LoadFile(filepath string) (err error) {
-	var file *os.File
-	if file, err = os.Open(filepath); err != nil {
-		return
-	}
-	return dbViper.ReadConfig(file)
+	return readInRuntime(&filepath)
 }
 
 func (rl *RuntimeLock) Save() (err error) {
+	dbViper.Set("pid", rl.Pid)
 	return dbViper.WriteConfig()
 }
 
