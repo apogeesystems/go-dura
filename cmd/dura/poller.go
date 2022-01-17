50f0952ff49c15a264878fca940906521e367dc1
Dane Nelson <dnelson0756@gmail.com>
2022-01-17 13:51:06 -0700

    Implemented initial runtimeLock logic, refined sleep timer to use configuration value (reset if invalid value given)

--- cmd/dura/poller.go
+++ cmd/dura/poller.go
@@ -8,9 +8,9 @@ import (
 	"time"
 )
 
-var (
-	runtimeLock RuntimeLock
-)
+//func init() {
+//	runtimeLock = RuntimeLock{}
+//}
 
 func processDirectory(currentPath string) (err error) {
 	var (
@@ -33,36 +33,48 @@ func processDirectory(currentPath string) (err error) {
 		if bytes, err = json.MarshalIndent(operation, "", "  "); err != nil {
 			return
 		}
-		fmt.Println(bytes)
+		fmt.Println(string(bytes))
 	}
 
 	return
 }
 
+// TODO fix PID issue so that it enforces only one instance of the poller
 func doTask() {
 	runtimeLock.Load()
-	if *runtimeLock.Pid != uint32(os.Getpid()) {
-		fmt.Fprintf(os.Stderr, "Shutting down because other poller took lock: %d", runtimeLock.Pid)
+	pid := uint32(os.Getpid())
+	if runtimeLock.Pid != nil && pid != *runtimeLock.Pid {
+		fmt.Fprintf(os.Stderr, "Shutting down because process %d is already running", runtimeLock.Pid)
 		os.Exit(0)
 	}
+	//if *runtimeLock.Pid != uint32(os.Getpid()) {
+	//	fmt.Fprintf(os.Stderr, "Shutting down because other poller took lock: %d\n\n", *runtimeLock.Pid)
+	//	os.Exit(0)
+	//}
 	var repo string
 	for repo, _ = range config.GitRepos() {
+		fmt.Printf("\nProcessing: '%s'\n", repo)
 		if err = processDirectory(repo); err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, err.Error())
 		}
 	}
 }
 
 func StartPoller() {
-	var err error
-	if err = runtimeLock.Load(); err != nil {
+	runtimeLock.Load()
+	pid := uint32(os.Getpid())
+	runtimeLock.Pid = &pid
+	if err = runtimeLock.Save(); err != nil {
 		log.Fatal(err)
 	}
-	*runtimeLock.Pid = uint32(os.Getpid())
-	runtimeLock.Save()
+	fmt.Printf("Dura PID: %d\n", pid)
 
+	if config.Dura.SleepSeconds < 1 {
+		fmt.Fprintf(os.Stderr, "Dura sleep seconds set to invalid value (<1) in config (%d), resetting to default value (%d)", config.Dura.SleepSeconds, DefSleepSeconds)
+		config.Dura.SleepSeconds = DefSleepSeconds
+	}
 	for {
 		doTask()
-		time.Sleep(5 * time.Second)
+		time.Sleep(time.Duration(config.Dura.SleepSeconds) * time.Second)
 	}
 }
