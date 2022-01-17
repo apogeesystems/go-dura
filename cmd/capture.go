79f7ef61200b7ffff0cf6b16303acabdf1ea4cf5
Dane Nelson <dnelson0756@gmail.com>
2022-01-16 20:23:43 -0700

    Changed command help descriptions

--- cmd/capture.go
+++ cmd/capture.go
@@ -24,16 +24,17 @@ import (
 // captureCmd represents the capture command
 var captureCmd = &cobra.Command{
 	Use:   "capture",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Run a single backup of an entire repository.",
+	Long:  `Run a single backup of an entire repository. This is the one single iteration of the 'serve'' control loop.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 { // Use CWD
+
+		} else { // Use paths provided
+
+		}
 		fmt.Println("capture called")
 	},
+	Version: "0.0.1",
 }
 
 func init() {
