676cbe1c96e7b33f4d34954aeefc0bd9dea43c8e
Dane Nelson <dnelson0756@gmail.com>
2022-01-16 20:24:42 -0700

    Removed (commented out) configuration initialization logic

--- cmd/root.go
+++ cmd/root.go
@@ -16,14 +16,17 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
+	"github.com/apogeesystems/go-dura/cmd/dura"
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	CWD     string
+	cfgFile string
+	err     error
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -43,14 +46,18 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err = rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(dura.InitConfig)
+
+	// Get current working directory
+	CWD, err = os.Getwd()
+	cobra.CheckErr(err)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -64,25 +71,31 @@ func init() {
 }
 
 // initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".go-dura" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".go-dura")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
-}
+//func initConfig() {
+//	if cfgFile != "" {
+//		// Use config file from the flag.
+//		viper.SetConfigFile(cfgFile)
+//	} else {
+//		// Find home directory.
+//		home, err := os.UserHomeDir()
+//		cobra.CheckErr(err)
+//
+//		// Search config in home directory with name ".go-dura" (without extension).
+//		viper.AddConfigPath(home)
+//		viper.SetConfigType("yaml")
+//		viper.SetConfigName(".go-dura")
+//	}
+//
+//	viper.AutomaticEnv() // read in environment variables that match
+//	viper.SetDefault("commit", map[string]interface{}{
+//		"author": nil,
+//		"email": nil,
+//		"exclude_git_config": false,
+//	})
+//	viper.SetDefault("repositories", map[string]dura.WatchConfig{})
+//
+//	// If a config file is found, read it in.
+//	if err := viper.ReadInConfig(); err == nil {
+//		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+//	}
+//}
